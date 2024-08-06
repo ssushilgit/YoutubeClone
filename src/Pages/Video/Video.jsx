e0e63c4893395fac76e03ff949e41d0bbb0c2aaa
ssushilgit <shresthasushil741@gmail.com>
2024-08-06 22:34:54 +0545

    Recommend Page

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import Recommend from '../../Components/Recommend/Recommend'
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo/>
+      <Recommend/>
 
     </div>
   )
