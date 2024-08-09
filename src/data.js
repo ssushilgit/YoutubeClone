5d564fb17588bd483ebea8f0904b31168208a651
ssushilgit <shresthasushil741@gmail.com>
2024-08-09 22:53:10 +0545

    value_converter function and displaying video on the feed accordingly

--- src/data.js
+++ src/data.js
@@ -1,1 +1,13 @@
-const API_KEY  = 'AIzaSyBMrOs6LeM7KOo0aIoH5fdvPOXGI2-sU6c'
+export const API_KEY  = 'AIzaSyBMrOs6LeM7KOo0aIoH5fdvPOXGI2-sU6c'
+
+export const value_converter = (value) =>{
+    if(value >= 1000000){
+        return Math.floor(value/1000000)+ "M";
+    }
+    else if(value >= 1000){
+        return Math.floor(value/1000)+"K";
+    }
+    else{
+        return value;
+    }
+}
